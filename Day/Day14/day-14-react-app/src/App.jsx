911f1b8b2db6ab6db4dcad985fc70714d9f25835
sarvesh <elemmisarvesh7@gmail.com>
2026-08-20 11:32:22 +0530

    list and keys 1

--- Day/Day14/day-14-react-app/src/App.jsx
+++ Day/Day14/day-14-react-app/src/App.jsx
@@ -1,33 +1,21 @@
-import { Welcome } from "./components/P1";
-import { ProductCard } from "./components/P2";
-import headphones from "./assets/sony.jpg";
-import ButtonDemo from "./components/P3";
-import Counter from "./components/StateExampleOne";
-import ToggleText from "./components/StateExampleTwo";
-import NameInput from "./components/StateExampleThree";
-import "./App.css";
+import './App.css'
+import ClickDemo from "./components/EventHandlerOne";
+import FormDemo from "./components/EventHandlerTwo";
+import ActionButtonDemo from "./components/EventHandlerThree";
 
+import SimpleList from "./components/ListAndKeysOne";
 function App() {
   return (
     <>
-      <NameInput/>
-      <NameInput/>
-      <ToggleText />
+      <ClickDemo/>
+      
+      <FormDemo />
 
-      <Counter />
+      <ActionButtonDemo />
 
-      <Welcome name="Rakesh" />
-
-      <ButtonDemo />
-
-      <ProductCard
-        //  name="Sony ULT Wear Noise Cancellation Wireless Bluetooth Over Ear Headphones with Massive Bass"
-        price={15999}
-        //  image="https://m.media-amazon.com/images/I/610DB8Cwm7L._SY450_.jpg"
-        image={headphones}
-      />
+      <SimpleList/>
     </>
-  );
+  )
 }
 
 export default App;
